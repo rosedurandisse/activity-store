b0bd29adbba7b4032003f4752f0c7fdaa678f957
Rose Durandisse <rosedurandisse@pursuit.org>
2022-02-18 10:41:28 -0500

    Added the schema and sql

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,11 +1,23 @@
-DROP DATABASE IF EXISTS cta_dev;
-CREATE DATABASE cta_dev;
+DROP DATABASE IF EXISTS  adventure_shop_dev;
+CREATE DATABASE adventure_shop_dev;
 
-\c cta_dev;
+\c  adventure_shop_dev;
 
-DROP TABLE IF EXISTS test;
+DROP TABLE IF EXISTS adventures;
 
-CREATE TABLE test (
+CREATE TABLE adventures (
     id SERIAL PRIMARY KEY, 
-    name TEXT
+    name TEXT,
+    description TEXT,
+    price INT,
+    rating INT,
+    CHECK (rating >= 0 AND rating <= 5),
+    adrenaline_approved BOOLEAN
 );
+
+
+-- name - a string, required
+-- description - a string
+-- price - a number
+-- rating - a number between 0 and 5
+-- featured - a boolean
--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,10 +1,7 @@
-\c cta_dev;
+\c  adventure_shop_dev;
 
-INSERT INTO test (name) VALUES
-('Monday'),
-('Tuesday'),
-('Wednesday'),
-('Thursday'),
-('Friday'),
-('Saturday'),
-('Sunday');
+INSERT INTO adventures (name, description, price, rating, adrenaline_approved) VALUES
+('Bungee Jumping', 'Dive off the cliffs with a rubber band as your only savior', 200, 3, true),
+('Skydiving', 'Jump out of a small plane to your assumed death with someone on your back', 150, 2, false),
+('Catamaran', 'Have a fun time with your friends and strangers with bottom shelf liqour and deli meats', 70, 2, false),
+('Picnic in the Park', 'Spend an afternoon with a future mate as ants make their way onto your pants',50,1,false);
