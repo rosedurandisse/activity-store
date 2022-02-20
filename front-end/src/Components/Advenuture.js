dfcee2506ad46176520a1bf07a8d465d0bbfa792
Rose Durandisse <rosedurandisse@pursuit.org>
2022-02-20 12:42:45 -0500

    Basic render of index page

--- front-end/src/Components/Advenuture.js
+++ front-end/src/Components/Advenuture.js
@@ -1,5 +1,10 @@
-function Adventure() {
-  return <div>Each Adventure</div>;
+function Adventure({ adventure }) {
+  console.log(adventure);
+  return (
+    <div>
+      <h1>{adventure.name}</h1>
+    </div>
+  );
 }
 
 export default Adventure;
