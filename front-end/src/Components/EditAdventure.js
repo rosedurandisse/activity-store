9941827d0f20d2afb0caaeaad1224d73bed61f5d
Rose Durandisse <rosedurandisse@pursuit.org>
2022-02-23 16:36:18 -0500

    Option Changes

--- front-end/src/Components/EditAdventure.js
+++ front-end/src/Components/EditAdventure.js
@@ -93,12 +93,16 @@ function EditAdventure() {
 
         <label htmlFor="rating">Rating:</label>
         <select
+          defaultValue=""
           name="rating"
           id="rating"
           value={adventure.rating}
           onChange={handleTextChange}
           required
         >
+          <option value="" disabled>
+            Select a Rating
+          </option>
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
